9913fda3c71a2287787a176eb105bfeb9936d602
Bnyro <bnyro@tutanota.com>
2023-02-07 10:15:47 +0100

    Switch to using the wikipedia api

--- engines/wiki.go
+++ engines/wiki.go
@@ -3,38 +3,40 @@ package engines
 import (
 	"errors"
 	"fmt"
-	"strings"
+	"net/url"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/bnyrogo/entities"
 	"github.com/bnyrogo/web"
 )
 
-const wikilessUrl = "https://wiki.adminforge.de"
+const wikiUrl = "https://en.wikipedia.org"
+
+
 func FetchWiki(query string) (entities.Wiki, error) {
-	uri := fmt.Sprintf("%s/wiki/%s?lang=en", wikilessUrl, query)
+	filter := url.QueryEscape("extracts|pageimages")
+	q := url.QueryEscape(query)
+	uri := fmt.Sprintf("%s/w/api.php?format=json&action=query&prop=%s&exintro&explaintext&redirects=1&pithumbsize=500&titles=%s", wikiUrl, filter, q)
 	result := entities.Wiki{
 		Url: uri,
 	}
 
-	doc, err := web.RequestHtml(uri)
+	var data map[string]interface{}
+	err := web.RequestJson(uri, &data)
 
 	if err != nil {
 		return result, err
 	}
 
-	
-	thumbnail, exists := doc.Find(".thumbimage").First().Attr("src")
-	if exists {
-		result.Thumbnail = wikilessUrl + thumbnail
-	}
-	
-	doc.Find(".mw-parser-output").Children().Each(func(i int, s *goquery.Selection) {
-		if result.Description != "" || strings.TrimSpace(s.Text()) == "" { return }
-		if s.Is("p") && !s.HasClass("mv-empty-elt") {
-			result.Description = s.Text()[:500] + " ..."
+	pages := data["query"].(map[string]interface{})["pages"].(map[string]interface{})
+	for key, value := range pages {
+		if key == "-1" { break }
+		entry := value.(map[string]interface{})
+		result.Description = entry["extract"].(string)
+		switch entry["thumbnail"].(type) {
+		case map[string]interface{}: result.Thumbnail = entry["thumbnail"].(map[string]interface{})["source"].(string)
+		default:
 		}
-	})
+	}
 
 	if (result.Description == "") { return result, errors.New("Not found") }
 
