c3fd7cfb65e74c431efa52ba576d6e057aabf84f
Bnyro <bnyro@tutanota.com>
2023-04-29 12:19:30 +0200

    Fix json conversion issues by using go structs

--- engines/dict.go
+++ engines/dict.go
@@ -8,11 +8,40 @@ import (
 	"github.com/bnyro/findx/web"
 )
 
+type DictResponse []struct {
+	Word      string `json:"word"`
+	Phonetics []struct {
+		Audio     string `json:"audio"`
+		SourceURL string `json:"sourceUrl,omitempty"`
+		License   struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"license,omitempty"`
+		Text string `json:"text,omitempty"`
+	} `json:"phonetics"`
+	Meanings []struct {
+		PartOfSpeech string `json:"partOfSpeech"`
+		Definitions  []struct {
+			Definition string `json:"definition"`
+			Synonyms   []any  `json:"synonyms"`
+			Antonyms   []any  `json:"antonyms"`
+			Example    string `json:"example"`
+		} `json:"definitions"`
+		Synonyms []string `json:"synonyms"`
+		Antonyms []any    `json:"antonyms"`
+	} `json:"meanings"`
+	License struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"license"`
+	SourceUrls []string `json:"sourceUrls"`
+}
+
 func FetchDictionary(query string) (entities.Dict, error) {
 	var dict entities.Dict
 
 	uri := fmt.Sprintf("https://api.dictionaryapi.dev/api/v2/entries/en/%s", query)
-	var data []map[string]interface{}
+	var data DictResponse
 
 	err := web.RequestJson(uri, &data)
 
@@ -24,25 +53,13 @@ func FetchDictionary(query string) (entities.Dict, error) {
 		return dict, errors.New("not found")
 	}
 
-	meanings := data[0]["meanings"].([]interface{})
-
 outer:
-	for _, entry := range meanings {
-		switch entry.(type) {
-		case map[string]interface{}:
-			{
-				meaning := entry.(map[string]interface{})
-				dict.PartOfSpeech = meaning["partOfSpeech"].(string)
-				for _, definition := range meaning["definitions"].([]interface{}) {
-					def := definition.(map[string]interface{})
-					dict.Definition = def["definition"].(string)
-					val, ok := def["example"]
-					if ok {
-						dict.Example = val.(string)
-					}
-					break outer
-				}
-			}
+	for _, entry := range data[0].Meanings {
+		dict.PartOfSpeech = entry.PartOfSpeech
+		for _, definition := range entry.Definitions {
+			dict.Definition = definition.Definition
+			dict.Example = definition.Example
+			break outer
 		}
 	}
 
--- engines/qwant.go
+++ engines/qwant.go
@@ -9,11 +9,105 @@ import (
 	"github.com/bnyro/findx/web"
 )
 
+type QwantImageResponse struct {
+	Status string `json:"status"`
+	Data   struct {
+		Query struct {
+			Locale       string `json:"locale"`
+			Query        string `json:"query"`
+			Offset       int    `json:"offset"`
+			QueryContext struct {
+				OriginalQuery string `json:"originalQuery"`
+			} `json:"queryContext"`
+		} `json:"query"`
+		Result struct {
+			Total int `json:"total"`
+			Items []struct {
+				Title         string `json:"title"`
+				Media         string `json:"media"`
+				Thumbnail     string `json:"thumbnail"`
+				ThumbWidth    int    `json:"thumb_width"`
+				ThumbHeight   int    `json:"thumb_height"`
+				Width         int    `json:"width"`
+				Height        int    `json:"height"`
+				URL           string `json:"url"`
+				ID            string `json:"_id"`
+				MediaFullsize string `json:"media_fullsize"`
+				MediaPreview  string `json:"media_preview"`
+				Size          string `json:"size"`
+				ThumbType     string `json:"thumb_type"`
+			} `json:"items"`
+			Instrumentation struct {
+				PingURLBase     string `json:"pingUrlBase"`
+				PageLoadPingURL string `json:"pageLoadPingUrl"`
+			} `json:"instrumentation"`
+			Filters struct {
+				Size struct {
+					Label    string `json:"label"`
+					Name     string `json:"name"`
+					Type     string `json:"type"`
+					Selected string `json:"selected"`
+					Values   []struct {
+						Value     string `json:"value"`
+						Label     string `json:"label"`
+						Translate bool   `json:"translate"`
+					} `json:"values"`
+				} `json:"size"`
+				License struct {
+					Label    string `json:"label"`
+					Name     string `json:"name"`
+					Type     string `json:"type"`
+					Selected string `json:"selected"`
+					Values   []struct {
+						Value     string `json:"value"`
+						Label     string `json:"label"`
+						Translate bool   `json:"translate"`
+					} `json:"values"`
+				} `json:"license"`
+				Freshness struct {
+					Label    string `json:"label"`
+					Name     string `json:"name"`
+					Type     string `json:"type"`
+					Selected string `json:"selected"`
+					Values   []struct {
+						Value     string `json:"value"`
+						Label     string `json:"label"`
+						Translate bool   `json:"translate"`
+					} `json:"values"`
+				} `json:"freshness"`
+				Color struct {
+					Label    string `json:"label"`
+					Name     string `json:"name"`
+					Type     string `json:"type"`
+					Selected string `json:"selected"`
+					Values   []struct {
+						Value     string `json:"value"`
+						Label     string `json:"label"`
+						Translate bool   `json:"translate"`
+					} `json:"values"`
+				} `json:"color"`
+				Imagetype struct {
+					Label    string `json:"label"`
+					Name     string `json:"name"`
+					Type     string `json:"type"`
+					Selected string `json:"selected"`
+					Values   []struct {
+						Value     string `json:"value"`
+						Label     string `json:"label"`
+						Translate bool   `json:"translate"`
+					} `json:"values"`
+				} `json:"imagetype"`
+			} `json:"filters"`
+			LastPage bool `json:"lastPage"`
+		} `json:"result"`
+	} `json:"data"`
+}
+
 const resultsPerPage = 25
 
 func FetchImage(query string, page int) ([]entities.Image, error) {
 	var images []entities.Image
-	var data map[string]interface{}
+	var data QwantImageResponse
 	offset := (page - 1) * resultsPerPage
 
 	if offset+resultsPerPage >= 250 {
@@ -27,14 +121,13 @@ func FetchImage(query string, page int) ([]entities.Image, error) {
 		return images, err
 	}
 
-	results := data["data"].(map[string]interface{})["result"].(map[string]interface{})["items"].([]interface{})
-	for _, res := range results {
-		result := res.(map[string]interface{})
-		image := entities.Image{}
-		image.Title = result["title"].(string)
-		image.Url = utilities.Redirect(result["url"].(string))
-		image.Thumbnail = utilities.RewriteProxied(result["thumbnail"].(string))
-		image.Media = result["media"].(string)
+	for _, result := range data.Data.Result.Items {
+		image := entities.Image{
+			Title:     result.Title,
+			Url:       utilities.Redirect(result.URL),
+			Thumbnail: utilities.RewriteProxied(result.Thumbnail),
+			Media:     result.Media,
+		}
 		images = append(images, image)
 	}
 
