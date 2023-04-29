15c7a68ef78456f448fb0c18f365d64ecb2eca69
Bnyro <bnyro@tutanota.com>
2023-04-29 12:28:53 +0200

    Improve wttr.in service response times

--- engines/wttr.go
+++ engines/wttr.go
@@ -3,10 +3,7 @@ package engines
 import (
 	"errors"
 	"fmt"
-	"strings"
-	"sync"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/bnyro/findx/utilities"
 	"github.com/bnyro/findx/web"
 )
@@ -16,51 +13,15 @@ func FetchWeather(query string) (string, error) {
 		return "", errors.New("query must contain alphabetic chars only")
 	}
 
-	uri := fmt.Sprintf("https://wttr.in/%s?T?0", query)
-	confirmUri := fmt.Sprintf("https://wttr.in/%s?format=j1", query)
+	uri := fmt.Sprintf("https://wttr.in/~%s?0", query)
 
 	var err error
-	var data web.Map
-	var doc *goquery.Document
 
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		web.RequestJson(confirmUri, &data)
-	}()
-	go func() {
-		defer wg.Done()
-		doc, err = web.RequestHtml(uri)
-	}()
-	wg.Wait()
+	doc, err := web.RequestHtml(uri)
 
 	if err != nil {
 		return "", err
 	}
 
-	areaName := extractValue(data, "areaName")
-	regionName := extractValue(data, "region")
-	countryName := extractValue(data, "country")
-
-	lowerQuery := strings.ToLower(query)
-	if areaName != lowerQuery && regionName != lowerQuery && countryName != lowerQuery {
-		return "", errors.New("invalid result")
-	}
-
-	weather, _ := doc.Find("pre").First().Html()
-	return weather, nil
-}
-
-func extractValue(data web.Map, key string) string {
-	if data["nearest_area"] == nil {
-		return ""
-	}
-	areas := data["nearest_area"].([]interface{})
-	if len(areas) == 0 {
-		return ""
-	}
-	area := areas[0]
-	value := area.(map[string]interface{})[key].([]interface{})[0]
-	return strings.ToLower(value.(map[string]interface{})["value"].(string))
+	return doc.Find("pre").First().Html()
 }
